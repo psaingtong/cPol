ce5acdb26207584c7dac663da9ffe35e01ccc0db
psaingtong <psaingtong@gmail.com>
2020-04-08 07:50:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+cPol
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/cPol_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc cPol public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(cPol_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    cPol_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions

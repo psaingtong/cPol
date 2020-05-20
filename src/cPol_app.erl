c827505bea4a00be74ea63fabad1a47ecef2243d
psaingtong <psaingtong@gmail.com>
2020-05-20 07:36:32 +0700

    init

--- src/cPol_app.erl
+++ src/cPol_app.erl
@@ -31,49 +31,20 @@ start(_StartType, _StartArgs) ->
     %cPol_ipd:test(),
     %%%
     %An="6300123",
-   %cPol_ipd:update_an(An),
+
     %Drg=cPol_drg:get_drg(An),
     %io:format("DRG--------~p~n",[Drg]),
     %%%
     %%Mdc - init
     FilePathMdc="data/mdc/22",
     %cPol_mdc:init(FilePathMdc),
-    %cPol_mdc:check_data(),
-    %cPol_mdc:import_data(),
-    %cPol_mdc:import_data_ax(),
-    Icd10=cPol_mdc:get_icd10("22:9915:ax_22pex"),
-    io:format("Icd10--------~p~n",[Icd10]),
+
+    %Icd10=cPol_mdc:get_icd10("22:9915:ax_22pex"),
+    %io:format("Icd10--------~p~n",[Icd10]),
     %%%%%
     %cPol_icd10:check_pdx("c602"),
 
-    %cPol_pcl:get_pdc(),
-    %cPol_pcl:initcc(),
-
     %%%%
-    %cPol_cc:get_value_in_range(""),
-
-    %cPol_cc:import_data(),
-
-    %cPol_icd10:list_data("/home/datawiz5/utth/cc/p050.csv"),
-    %cPol_icd10:import_code("/home/datawiz5/utth/ecode/e_dxcode6.csv"),
-    %cPol_test:test1(),
-    %Te1=cPol_db:get_an("6300123"),
-    %io:format("D Test--------~p~n",[Te1]),
-
-
-    %%%%
-    %FilePath="data/dcl/b",
-    %cPol_pcl:init(FilePath),
-    %FilePath="/home/datawiz5/utth/cc_app",
-    %FileName="k250.csv",
-    %FilePathName=string:join([FilePath, FileName], "/"),
-    %io:format("D Test--------~p~n",[FilePathName]),
-
-    %cPol_pcl:import_data(FilePath,FileName),
-
-    %Te3=cPol_pcl:get_code("i10:0011"),
-    %io:format("D Test--------~p~n",[Te3]),
-
     cPol_sup:start_link().
 
 stop(_State) ->
